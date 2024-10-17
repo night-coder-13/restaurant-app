0f74de20ebda2b053d2efda727899999e398ba69
Mehdi <nikanuser@gmail.com>
2024-10-17 22:59:52 +0330

    add feature

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,5 +1,5 @@
 @extends('layout.master')
-@section('title' , 'Home page')
+@section('title', 'Home page')
 
 @section('link')
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.8.0/dist/leaflet.css"
@@ -30,48 +30,8 @@
     </div>
     </div>
 
-    <section class="card-area layout_padding">
-        <div class="container">
-            <div class="row gy-5">
-                <div class="col-md-4 col-sm-6 col-xs-6">
-                    <div class="card text-center">
-                        <div class="card-body">
-                            <div class="card-icon-wrapper">
-                                <i class="bi bi-telephone-fill fs-2 text-white card-icon"></i>
-                            </div>
-                            <p class="card-text fw-bold">لورم ایپسوم متن ساختگی</p>
-                            <p class="card-text">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده
-                                از طراحان گرافیک است. </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4 col-sm-6 col-xs-6">
-                    <div class="card text-center">
-                        <div class="card-body">
-                            <div class="card-icon-wrapper">
-                                <i class="bi bi-clock-fill fs-2 text-white card-icon"></i>
-                            </div>
-                            <p class="card-text fw-bold">لورم ایپسوم متن ساختگی</p>
-                            <p class="card-text">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده
-                                از طراحان گرافیک است. </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4 col-sm-6 col-xs-6">
-                    <div class="card text-center">
-                        <div class="card-body">
-                            <div class="card-icon-wrapper">
-                                <i class="bi bi-geo-alt-fill fs-2 text-white card-icon"></i>
-                            </div>
-                            <p class="card-text fw-bold">لورم ایپسوم متن ساختگی</p>
-                            <p class="card-text">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده
-                                از طراحان گرافیک است. </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+    @include('home.feature')
+
 
     <!-- food section -->
     <section class="food_section layout_padding-bottom">
